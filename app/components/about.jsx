ccb8be0c7fed43d35c0ef9b652936724450392cd
Garon <garoncdavis@gmail.com>
2016-07-12 08:31:08 -0400

    added styles to About

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -3,8 +3,13 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page</p>
+      <h1 className="text-center">About</h1>
+      <p className="text-center">Welcome to the About page</p>
+      <ul className="text-center" style={{listStyleType:'none'}}>
+        <li>React weather app</li>
+        <li>API appears to be broken for now</li>
+        <li>Also webpack was involved</li>
+      </ul>
     </div>
   )
 };
