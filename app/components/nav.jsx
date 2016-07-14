544e9c38bd1d906fe4d75ba29801534b6b7daa5b
Garon <garoncdavis@gmail.com>
2016-07-14 16:58:08 -0400

    wired search functionailty to header

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -4,7 +4,14 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function (e) {
     e.preventDefault();
-    alert("Not yet wired up");
+
+    var location = this.refs.headerSearch.value;
+    var headerSearch = this.refs.headerSearch.value;
+    var encodedLocation = encodeURIComponent(location);
+    if (location.length > 0){
+      this.refs.headerSearch.value = "";
+      window.location.hash = '#/?location=' + encodedLocation;
+    }
   },
   render: function () {
     return (
@@ -21,7 +28,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search Weather by city"/>
+                <input ref="headerSearch" type="search" placeholder="Search Weather by city"/>
               </li>
               <li>
                 <input type="submit" className="button" value="Get Weather"/>
